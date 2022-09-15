96c8b7e27d573fa4f6a2ef8efe4353bbbc7d9448
KarolaBokeh <karolabokeh@protonmail.com>
2022-09-15 13:30:46 -0400

    added link attribute

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="#" className="btn btn-primary">
+        <a href="https://karennavarro.io/" className="btn btn-primary">
           Hello
         </a>
       </header>
