7c30140353a0e3e09c47677443e75135a06d3411
KarolaBokeh <karolabokeh@protonmail.com>
2022-09-23 09:19:14 -0400

    Created new photos component, added Image API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,26 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
 
     let pexelsApiKey =
       "563492ad6f917000010000017e18d0a9b25c4685bd8e2ae9a4e2997d";
-    let pexelsApiUrl =
-      "https://api.pexels.com/v1/search?query=nature&per_page=1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleKeywordChange(event) {
@@ -41,6 +46,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
